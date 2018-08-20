7d675c09b37234c814701b846dacc143fb971f26
Unknown <riversocesare6@gmail.com>
2018-08-20 16:52:06 -0500

    test

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'Patrick - vCard & Resume Template'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
+	$subj = "Cesare Portfolio'"; //letter subject
+	$to = 'riversocesare6@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
